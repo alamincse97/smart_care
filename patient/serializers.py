bb0ae289a38265f06173e6bf484b530f375e5b26
alamincse97 <alamin.cse97@gmail.com>
2024-08-24 12:23:42 +0600

    [ADD]: Create a Doctor Serializers

--- patient/serializers.py
+++ patient/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from . import models
 
 class PatientSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
     class Meta:
         model = models.Patient
         fields = '__all__'
