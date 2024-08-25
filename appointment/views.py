6b7ebb0e75290b6dfc8694e08a3108f3d12b6ac7
alamincse97 <alamin.cse97@gmail.com>
2024-08-25 15:41:04 +0600

    [UPDATE]: Create a Appointment Api Changed

--- appointment/views.py
+++ appointment/views.py
@@ -8,3 +8,11 @@ from . import serializers
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = models.Appointment.objects.all()
     serializer_class = serializers.AppointmentSerializer
+    
+    # Custom Query
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id:
+            queryset = queryset.filter(patient_id= patient_id)
+        return queryset
