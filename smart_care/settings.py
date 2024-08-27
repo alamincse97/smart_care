be246ee4c04c46070a7ff009888737dd94906da8
alamincse97 <alamin.cse97@gmail.com>
2024-08-27 23:22:51 +0600

    [ADD]: Some bug fix

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=8fq9vjc!@-idr%2os5azyocrtoyz%w#plmu&+9t-%*=+3tyaa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
